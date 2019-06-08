c54cb8322477ed46a6496f157f7f7b6672b8db1c
paulkarabilo <benizebe@gmail.com>
2019-06-08 22:43:22 +0200

    remove face cotext menu func

--- ui/__init__.py
+++ ui/__init__.py
@@ -3,17 +3,11 @@ import bpy
 from .face import FaceCapifier
 from .object import ObjectCapifier
 
-
-
 def special_menu_func(self, context):
     is_face_mode = context.tool_settings.mesh_select_mode[2]
     if is_face_mode:
         self.layout.separator()
         self.layout.operator(FaceCapifier.bl_idname)
-
-def face_menu_func(self, context):
-    self.layout.separator()
-    self.layout.operator(FaceCapifier.bl_idname)
 
 def object_menu_func(self, context):
     self.layout.separator()
@@ -24,11 +18,9 @@ def register():
     bpy.utils.register_class(ObjectCapifier)
     bpy.types.VIEW3D_MT_object_context_menu.append(object_menu_func)
     bpy.types.VIEW3D_MT_edit_mesh_context_menu.append(special_menu_func)
-    bpy.types.VIEW3D_MT_edit_mesh_faces_context_menu.append(face_menu_func)
 
 def unregister():
     bpy.types.VIEW3D_MT_edit_mesh_context_menu.remove(special_menu_func)
-    bpy.types.VIEW3D_MT_edit_mesh_faces_context_menu.remove(face_menu_func)
     bpy.types.VIEW3D_MT_object_context_menu.remove(object_menu_func)
     bpy.utils.unregister_class(ObjectCapifier)
     bpy.utils.unregister_class(FaceCapifier)
